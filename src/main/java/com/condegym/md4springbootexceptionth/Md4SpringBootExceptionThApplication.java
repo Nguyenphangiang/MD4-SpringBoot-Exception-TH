f0596ca33976138b43df7184dea6a261cb96b72c
Nguyenphangiang <phancruel@gmail.com>
2022-05-05 10:20:20 +0700

    up

---
+++ src/main/java/com/condegym/md4springbootexceptionth/Md4SpringBootExceptionThApplication.java
@@ -0,0 +1,13 @@
+package com.condegym.md4springbootexceptionth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4SpringBootExceptionThApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4SpringBootExceptionThApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/condegym/md4springbootexceptionth/Md4SpringBootExceptionThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condegym.md4springbootexceptionth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4SpringBootExceptionThApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
